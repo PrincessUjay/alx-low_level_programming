b661fe38491e8c4aa4db98f5acf59465ce54bb34
PrincessUjay <onyekabaprincess@gmail.com>
2023-04-20 17:01:22 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line with puts function
- * return: 0 (success)
+ * main -Entry point
+ *
+ * return: Always 0 (success)
  */
 int main(void)
 {

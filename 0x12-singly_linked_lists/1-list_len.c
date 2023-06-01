5bbc491836d2f6bdf3b5c9ea1a76d2d5f506389d
Princess Onyekaba <74983978+PrincessUjay@users.noreply.github.com>
2023-06-02 00:13:17 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,25 @@
-ww
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+    if (h == NULL)
+        return 0;
+
+    size_t count = 1;
+    const list_t *current = h;
+
+    while (current->next != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}
